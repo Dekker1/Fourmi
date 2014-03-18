4f5b66fff69b68666d4e114afa2d53382476b985
Jip J. Dekker <admin@recondor.com>
2014-03-18 17:28:49 +0100

    Basic structure to make sure the spider use an argument

--- Fourmi/spiders/Chemspider.py
+++ Fourmi/spiders/Chemspider.py
@@ -3,9 +3,10 @@ from scrapy.spider import Spider
 class ChemspiderSpider(Spider):
     name = "Chemspider"
     allowed_domains = ["chemspider.com"]
-    start_urls = (
-        'http://www.chemspider.com/',
-        )
+
+    def __init__(self, compound=None, *args, **kwargs):
+        super(ChemspiderSpider, self).__init__(*args, **kwargs)
+        self.start_urls = ["http://chemspiderapiurl/something/%s" % compound] #[TODO] - Give an logical start url.
 
     def parse(self, response):
         pass 
