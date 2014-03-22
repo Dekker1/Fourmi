306a37db1a9be535a0d624b8bf5e1004f218f43c
Jip J. Dekker <admin@recondor.com>
2014-03-22 15:48:08 +0100

    A better structure which is able to start multiple spiders.

--- Fourmi.py
+++ Fourmi.py
@@ -10,13 +10,20 @@ from scrapy import log, signals
 from FourmiCrawler.spiders.Chemspider import ChemspiderSpider # [review] - There should be an easy way to import all spiders!
 from scrapy.utils.project import get_project_settings
 
-# [todo] - Add something to add all spiders, with the right references
-spider = ChemspiderSpider(compound = "Aspirin")
-settings = get_project_settings()
-crawler = Crawler(settings)
-crawler.signals.connect(reactor.stop, signal=signals.spider_closed)
-crawler.configure()
-crawler.crawl(spider)
-crawler.start()
-log.start()
-reactor.run()
+defined_spiders = [ChemspiderSpider(compound = "Methane")]
+
+def setup_crawler(Spider, compound):
+  spider = FollowAllSpider(domain=domain) # [todo] - Do something smart to get the different spiders to work here.
+  settings = get_project_settings()
+  crawler = Crawler(settings)
+  crawler.configure()
+  crawler.crawl(spider)
+  crawler.start()
+
+def start():
+  for spider in defined_spiders:
+    setup_crawler(spider, compound)
+  log.start()
+  reactor.run()
+
+start()
