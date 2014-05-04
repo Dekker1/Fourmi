0cec4bd2d8960bb9ffa1e8b3f99f3230bb694580
Rob tB <r.tenberge@student.ru.nl>
2014-05-04 20:48:51 +0200

    new_compound_request now returns a Request with a searh URL

--- FourmiCrawler/sources/NIST.py
+++ FourmiCrawler/sources/NIST.py
@@ -8,6 +8,8 @@ from FourmiCrawler.items import Result
 class NIST(Source):
     website = "http://webbook.nist.gov/*"  
 
+    search = 'cgi/cbook.cgi?Name=%s&Units=SI&cTG=on&cTC=on&cTP=on'
+
     def __init__(self):
         Source.__init__(self)
 
@@ -15,5 +17,5 @@ class NIST(Source):
         pass
 
     def new_compound_request(self, compound):
-        # return Request(url=self.website[:-1] + compound, callback=self.parse)
-        pass
+        return Request(url=self.website[:-1] + self.search % compound,
+                       callback=self.parse)
