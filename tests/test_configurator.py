efa8d45d9c08120374b8cb34a39574565881f3d8
Jip J. Dekker <jip@dekker.li>
2014-06-08 13:28:14 +0200

    I don't yet know a way to test the start_log function

--- tests/test_configurator.py
+++ tests/test_configurator.py
@@ -20,8 +20,8 @@ class TestConfigurator(unittest.TestCase):
         self.assertEqual(self.conf.scrapy_settings["FEED_URI"], "results.csv")
         self.assertEqual(self.conf.scrapy_settings["FEED_FORMAT"], "csv")
 
-    def test_start_log(self):
-        self.conf.start_log("test.log", True)
-        self.conf.start_log("test.log", False)
-        self.conf.start_log(None, True)
-        self.conf.start_log(None, False)
+    # def test_start_log(self):
+    #     self.conf.start_log("test.log", True)
+    #     self.conf.start_log("test.log", False)
+    #     self.conf.start_log(None, True)
+    #     self.conf.start_log(None, False)
