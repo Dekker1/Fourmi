88a1390de8c7bf206d81495a7fea858dad3483b6
Harmen Prins <prins_harmen@hotmail.com>
2014-06-17 17:09:49 +0200

    Added asserts to the tests

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -12,6 +12,12 @@ class TestGUI(unittest.TestCase):
         self.test_gui.window.after(11, self.test_gui.window.destroy)
         self.test_gui.run()
 
+        output_type = self.test_gui.configurator.load_output_types().split(',')[0]
+
+        self.assertEqual(self.test_gui.values.get('substance'), '')
+        self.assertEqual(self.test_gui.values.get('output_type'), output_type)
+        self.assertEqual(self.test_gui.values.get('output_name'), 'results')
+
 
     def test_no_configurations(self):
         self.test_gui = gui.GUI(None)
@@ -21,7 +27,6 @@ class TestGUI(unittest.TestCase):
         self.test_gui.window.after(11, self.test_gui.window.destroy)
         self.test_gui.run()
 
-
-
-
-
+        self.assertEqual(self.test_gui.values.get('substance'), '')
+        self.assertEqual(self.test_gui.values.get('output_type'), 'csv')
+        self.assertEqual(self.test_gui.values.get('output_name'), 'results')
