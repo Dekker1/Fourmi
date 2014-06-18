5fbbb26c00a7de9b5204537f437f808ad4eaac1e
Harmen Prins <prins_harmen@hotmail.com>
2014-06-18 09:58:22 +0200

    GUI_Options -> GUI

--- GUI/configImporter.py
+++ GUI/configImporter.py
@@ -11,20 +11,20 @@ class ConfigImporter():
     def load_common_attributes(self):
         """Loads common attributes from the initialized file."""
         try:
-            return self.parser.get('GUI_Options', 'CommonParameters')
+            return self.parser.get('GUI', 'CommonParameters')
         except:
             return 'One, Two, Three'
 
     def load_output_types(self):
         """Loads output types from the initialized file."""
         try:
-            return self.parser.get('GUI_Options', 'OutputTypes')
+            return self.parser.get('GUI', 'OutputTypes')
         except:
             return 'csv'
 
     def load_always_attributes(self):
         """Loads attributes that are always searched for from the initialized file."""
         try:
-            return self.parser.get('GUI_Options', 'AlwaysParameters')
+            return self.parser.get('GUI', 'AlwaysParameters')
         except:
             return 'Name, Weight'
