0521139b12eb2fc8ed38b380d0f617f1dd139e12
Jip J. Dekker <jip@dekker.li>
2014-06-21 01:22:13 +0200

    The tests had old config file assumptions

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -7,7 +7,7 @@ class TestGUI(unittest.TestCase):
         pass
 
     def test_empty_attributes(self):
-        self.test_gui = gui.GUI(None, '../GUI.cfg.sample', in_source=False)
+        self.test_gui = gui.GUI(None, config_file="../GUI.cfg.sample", in_source=True)
         self.test_gui.window.after(9, self.test_gui.prepare_search)
         self.test_gui.window.after(11, self.test_gui.window.destroy)
         self.test_gui.run()
@@ -20,7 +20,7 @@ class TestGUI(unittest.TestCase):
 
 
     def test_no_configurations(self):
-        self.test_gui = gui.GUI(None)
+        self.test_gui = gui.GUI(None, config_file="../GUI.cfg.sample")
         self.test_gui.configurator = gui.ConfigImporter('')
         self.test_gui.finish_with_search = True
         self.test_gui.window.after(9, self.test_gui.prepare_search)
